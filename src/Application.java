ab9a389a889412faf006c62c1a4683f022743483
basilaleyas <baleyas@gmail.com>
2014-12-16 18:24:34 -0500

    Update

--- src/Application.java
+++ src/Application.java
@@ -1,29 +1,37 @@
 import java.util.Scanner;
 
-
+class Rcalculator {
+	
+	
+}
 public class Application {
 	public static void main(String[] args) {
 	
-	// Current Age
-	int cAge;
-	// Retirement Age
-	int rAge;
-	// Currently Saved Up Money
-	double cSave;
-	// Current Interest Rate for 
-	double cInte;
-	
-	
-	
-	
-	
-	
-	// Retirement Calculation
+	Rcalculator ba = new Rcalculator();
 	
 	Scanner input = new Scanner(System.in);
 	
-	System.out.println("Enter current Age: ");
-	userinput = input.nextLine();
+	// ca = current age, cs = current saving, ci = current interest rate 
+	// efi = Future interest rate fi = future income, yi = yearly income 
+	double ca, cs, ci, efi, fi, yi;
+	
+	
+	//Current Age, Output
+	System.out.println("Enter your current Age: ");
+	ca = input.nextDouble();
+	//Currently saved Output
+	System.out.println("Enter your current saving: ");
+	cs = input.nextDouble();
+	//Current Interest rate.
+	System.out.println();
+	ci = input.nextDouble();
+	// Expected Future Interest rate
+	System.out.println("Expected maximum interest rate: ");
+	
+	
+	
+	//Wait for user to enter Age
+	String currentAge = input.nextLine();
 
 	}
 }
