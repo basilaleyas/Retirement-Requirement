a26e31066fed4ff7a40bff22b7e3bff55d29b8ea
basilaleyas <baleyas@gmail.com>
2014-12-15 23:24:28 -0500

    Retirement Calculator

---
+++ src/Application.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+
+public class Application {
+	public static void main(String[] args) {
+	
+	// Current Age
+	int cAge;
+	// Retirement Age
+	int rAge;
+	// Currently Saved Up Money
+	double cSave;
+	// Current Interest Rate for 
+	double cInte;
+	
+	
+	
+	
+	
+	
+	// Retirement Calculation
+	
+	Scanner input = new Scanner(System.in);
+	
+	System.out.println("Enter current Age: ");
+	userinput = input.nextLine();
+
+	}
+}
