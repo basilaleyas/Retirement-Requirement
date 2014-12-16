dc2bde748d2efda1ccca0ab37046e013fad05a96
basilaleyas <baleyas@gmail.com>
2014-12-16 18:55:41 -0500

    update

--- src/Application.java
+++ src/Application.java
@@ -11,27 +11,37 @@ public class Application {
 	
 	Scanner input = new Scanner(System.in);
 	
-	// ca = current age, cs = current saving, ci = current interest rate 
-	// efi = Future interest rate fi = future income, yi = yearly income 
-	double ca, cs, ci, efi, fi, yi;
-	
+	// ca = current age, ra = retirement age, cs = current saving, ci = current interest rate 
+	// efi = Maximum future interest rate, emi = Minimum future interest rate
+	//fi = future income, yi = yearly income 
+	double cs, ci, efi,emi, fi, yi;
+	int ca, ra;
 	
 	//Current Age, Output
 	System.out.println("Enter your current Age: ");
-	ca = input.nextDouble();
+	ca = input.nextInt();
+	//Year expected to retirement age
+	System.out.println("Retirement Age: " );
+	ra = input.nextInt();
 	//Currently saved Output
 	System.out.println("Enter your current saving: ");
 	cs = input.nextDouble();
 	//Current Interest rate.
-	System.out.println();
+	System.out.println("Enter current interest rate: ");
 	ci = input.nextDouble();
-	// Expected Future Interest rate
-	System.out.println("Expected maximum interest rate: ");
+	// Maximum Future Interest rate
+	System.out.println("Maximum interest rate: ");
+	efi = input.nextDouble();
+	//Minimum interest rate
+	System.out.println("Mimum Interest rate: ");
+	emi = input.nextDouble();
 	
 	
 	
-	//Wait for user to enter Age
-	String currentAge = input.nextLine();
+	
+	
+	
+
 
 	}
 }
